e934a5b410d1ddfff7b7fbc2b624c96b8b3092e5
Sabyasachi <sabyasachirakshitacs19@gmail.com>
2026-05-06 21:51:45 +0530

    swap order of Create and Passbook navigation items in BottomNav

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -8,6 +8,15 @@ const NAV_ITEMS = [
       </svg>
     ),
   },
+   {
+    key: 'create',
+    label: 'Add Record',
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
+      </svg>
+    ),
+  },
   {
     key: 'passbook',
     label: 'Passbook',
@@ -17,15 +26,7 @@ const NAV_ITEMS = [
       </svg>
     ),
   },
-  {
-    key: 'create',
-    label: 'Add Record',
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" className="w-6 h-6">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
-      </svg>
-    ),
-  },
+ 
 ]
 
 export default function BottomNav({ activePage, setActivePage }) {
